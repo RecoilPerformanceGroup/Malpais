c87319e14f4403ca37aa99f6accb84d107ed2024
Recoil Mac Pro <jonas@halfdanj.dk>
2011-02-10 19:21:16 +0100

    Leaking

--- Plugins/Leaking/Leaking.mm
+++ Plugins/Leaking/Leaking.mm
@@ -1,19 +1,35 @@
 #import "Leaking.h"
 #import "Kinect.h"
 #import "Keystoner.h"
+#include <dispatch/dispatch.h>
 
 
 @implementation Rubber
 
-@synthesize elasticForce, damping, pullForce, speed;
+@synthesize elasticForce, damping, pullForce, speed, border, pushForceInternal, pushForceExternal, pushForceInternalDist, pushForceExternalDist;;
 
--(id) init{
-	if([super init]){
+-(id) initWithPoints:(vector<ofxPoint2f>) _points{
+	if([self init]){
+		//Find center
+		ofxPoint2f center;
+		for(int i=0;i<_points.size();i++){
+			center += _points[i];
+		}
+		center /= _points.size();
+		
 		for(int i=0;i<NUMPOINTS;i++){
 			float r = TWO_PI*(float)i/NUMPOINTS;
-			ofxPoint2f p = 	ofxPoint2f(0.2,0.5) + ofxPoint2f(sin(r), cos(r))*0.3;
+			ofxPoint2f p = 	center + ofxPoint2f(sin(r), cos(r))*0.03;
 			RPoint newP;
 			newP.pos = p;
+			
+			newP.filterX.setNl(9.413137469932821686e-04, 2.823941240979846506e-03, 2.823941240979846506e-03, 9.413137469932821686e-04);
+			newP.filterX.setDl(1, -2.5818614306773719263, 2.2466666427559748864, -.65727470210265670262);
+			newP.filterY.setNl(9.413137469932821686e-04, 2.823941240979846506e-03, 2.823941240979846506e-03, 9.413137469932821686e-04);
+			newP.filterY.setDl(1, -2.5818614306773719263, 2.2466666427559748864, -.65727470210265670262);
+			
+			newP.filterX.setStartValue(p.x);
+			newP.filterY.setStartValue(p.y);			
 			
 			points.push_back(newP);
 		}
@@ -21,33 +37,74 @@
 	return self;
 }
 
--(void) updateWithPoints:(vector<ofxPoint2f>) pointsIn{
-	//points.clear();
-	/*	for(int i=0;i<pointsIn.size();i++){
-	 if(i >= points.size()){
-	 points.push_back(pointsIn[i]);
-	 }
-	 }*/
-	vector<ofPoint> poly;
+//Returns stroked point
+-(ofxPoint2f) innerPoint:(int)i{
+	RPoint * point = &points[i];
+	RPoint * prevPoint, * nextPoint;
+	if(i == 0){
+		prevPoint = &points[points.size()-1];
+		nextPoint = &points[i+1];
+	} else if(i == points.size()-1){
+		prevPoint = &points[i-1];
+		nextPoint = &points[0];		
+	} else {
+		prevPoint = &points[i-1];
+		nextPoint = &points[i+1];		
+	}
+	
+	ofxVec2f v = nextPoint->pos - prevPoint->pos;
+	v = -ofxVec2f(-v.y,v.x).normalized()*[[self border] floatValue]*0.1;
+	
+	return point->pos+v;
+	
+}
+
+-(ofxPoint2f) centroid{
+	ofxPoint2f c;
 	for(int i=0;i<points.size();i++){
+		c += points[i].pos;	
+	}
+	c /= points.size();
+	return c;
+}
+
+-(void) updateWithPoints:(vector<ofxPoint2f>) pointsIn{
+	
+	int pointsJumper = 1;
+	
+	vector<ofPoint> poly;
+	for(int i=0;i<points.size();i+=pointsJumper){
 		poly.push_back(points[i].pos);
 	}
 	
-	for(int i=0;i<pointsIn.size();i++){
-		if(!ofInsidePoly(pointsIn[i].x, pointsIn[i].y, poly)){
+	const int N = pointsIn.size();	
+	bool inPoly[N];
+	memset(inPoly, 0, N);
+	
+	
+	for(int i=0;i<N;i++){		
+		if(ofInsidePoly(pointsIn[i].x, pointsIn[i].y, poly)){
+			inPoly[i] = true;
+		}
+	}		
+	
+	for(int i=0;i<N;i++){
+		if(!inPoly[i]){
 			//Find nærmeste punkt
 			float bestDist = -1;
 			RPoint * bestPoint;
-			for(int j=0;j<points.size();j++){
-				ofxVec2f v = points[j].pos - pointsIn[i];
-				if(bestDist == -1 || bestDist > v.length()){
+			for(int j=0;j<points.size();j+=pointsJumper){
+				//				ofxVec2f v = points[j].pos - pointsIn[i];
+				//				float d = v.lengthSquared();
+				float d = points[j].pos.distanceSquared(pointsIn[i]) ;
+				if(bestDist == -1 || bestDist > d){
 					bestPoint = &points[j];
-					bestDist = v.length();
+					bestDist = d;
 				}
 			}
 			if(bestPoint != nil){
 				ofxVec2f v = bestPoint->pos - pointsIn[i];
-				bestPoint->f += -v*[[self pullForce] floatValue]*1.0/(pointsIn.size()/100.0);
+				bestPoint->f += -0.01*v*[[self pullForce] floatValue]*1.0/(pointsIn.size()/100.0);
 			}
 		} 
 	}
@@ -55,59 +112,144 @@
 	lastPointsIn = pointsIn;
 }
 
+-(void) updateForceToOtherObjects:(NSArray*)array{
+	float internalPushForceDist = [[self pushForceInternalDist] floatValue];
+	float externalPushForceDist = [[self pushForceExternalDist] floatValue];
+	float internalPushForce = [[self pushForceInternal] floatValue];
+	float externalPushForce = [[self pushForceExternal] floatValue];
+	
+	//Find punkter der er inde i den anden
+	int pointsJumper = 5;
+	vector<ofPoint> poly;
+	for(int i=0;i<points.size();i+=pointsJumper){
+		poly.push_back(points[i].pos);
+	}
+	
+	
+	for (Rubber * obj in array) {
+		if(obj != self){
+			for(int u=0;u<obj->points.size();u++){
+				ofxPoint2f center = [obj centroid];
+				RPoint * pThat = &obj->points[u];
+				bool centerInPoly = ofInsidePoly(center.x, center.y, poly);
+				if(ofInsidePoly(pThat->pos.x, pThat->pos.y, poly)){
+					ofxPoint2f goal;
+					if(centerInPoly){
+						goal = ofxPoint2f(aspect/2.0 , 0);
+					} else {
+						goal = center;
+					}
+					pThat->f += 0.01*(goal - pThat->pos);
+				}
+			}
+		}
+		
+		for(int i=0;i<points.size();i++){
+			RPoint * pThis = &points[i];
+			for(int u=0;u<obj->points.size();u++){
+				RPoint * pThat = &obj->points[u];
+				if(pThis != pThat){
+					ofxVec2f v = pThis->pos - pThat->pos;
+					float d = v.length();
+					if(obj == self){
+						if(d < internalPushForceDist){
+							d *= 1.0/internalPushForceDist;
+							
+							float f = -d+1;
+							v.normalize();
+							float s = 0.00001;
+							pThat->f -= f*v*s*internalPushForce;		
+							
+						}
+						
+					} else {
+						if(d < externalPushForceDist){
+							d *= 1.0/externalPushForceDist;
+							
+							float f = -d+1;
+							v.normalize();
+							float s = 0.0001;
+							pThat->f -= f*v*s*externalPushForce;		
+							
+						}
+						
+						
+					}
+				}				
+			}
+		}
+	}
+}
+
 -(void) updateWithTimestep:(float)time{
 	
-	for(int j=0;j<200;j++){
-		RPoint * prevPoint = &points[points.size()-1];
-		for(int i=0;i<points.size();i++){
-			RPoint * point = &points[i];	
-			
-			ofxVec2f v = prevPoint->pos - point->pos;
-			
-			v *= [[self elasticForce] floatValue]*1.0/20.0;
-			
-			point->f += v;
-			prevPoint->f -= v;
-			
-			prevPoint = point;
-		}	
+	RPoint * prevPoint = &points[points.size()-1];
+	for(int i=0;i<points.size();i++){
+		RPoint * point = &points[i];	
 		
+		ofxVec2f v = prevPoint->pos - point->pos;
 		
-		for(int i=0;i<points.size();i++){
-			RPoint * point = &points[i];	
-			point->v *= [[self damping] floatValue];
-			point->v += point->f*[[self speed] floatValue];
-			
-			point->v.limit(0.01);
-			
-			point->pos += point->v*time;
-			
-			
-		}	
-	}
+		v *= [[self elasticForce] floatValue]*1.0/20.0;
+		
+		point->f += v;
+		prevPoint->f -= v;
+		
+		prevPoint = point;
+	}	
+	
+	
+	for(int i=0;i<points.size();i++){
+		RPoint * point = &points[i];	
+		point->v *= [[self damping] floatValue];
+		point->v += point->f*[[self speed] floatValue];
+		
+		point->v.limit(0.01);
+		
+		ofxPoint2f p = point->pos + point->v*time;
+		p.x = ofClamp(p.x, 0, aspect);
+		p.y = ofClamp(p.y, 0, 1);
+		
+		point->pos = p;		
+	}	
 	
 	for(int i=0;i<points.size();i++){
 		RPoint * point = &points[i];
 		point->f = ofxVec2f();		
 	}
-
+	
 }	
 
+-(void) calculateFilteredPos{
+	for(int i=0;i<points.size();i++){
+		RPoint * point = &points[i];
+		
+		point->filteredPos.x =  point->filterX.filter(point->pos.x);
+		point->filteredPos.y =  point->filterY.filter(point->pos.y);		
+	}	
+}
+
 -(void) draw{
-	ofSetColor(255, 255, 255);
+	ofSetColor(0, 100, 0);
 	glBegin(GL_LINE_STRIP);
 	for(int i=0;i<points.size();i++){
 		glVertex2d(points[i].pos.x, points[i].pos.y);
 	}
 	glEnd();
 	
-	ofSetColor(255, 0, 0);
+	ofSetColor(255, 255, 255);
+	glBegin(GL_LINE_STRIP);
+	for(int i=0;i<points.size();i++){
+		glVertex2d(points[i].filteredPos.x, points[i].filteredPos.y);
+	}
+	glEnd();
+	
+	/*ofSetColor(255, 0, 0);
 	
 	glBegin(GL_POINTS);
 	for(int i=0;i<lastPointsIn.size();i++){
 		glVertex2d(lastPointsIn[i].x, lastPointsIn[i].y);
 	}
-	glEnd();
+	glEnd();*/
 }
 
 @end
@@ -117,12 +259,28 @@
 
 -(void) initPlugin{
 	[self addProperty:[NumberProperty sliderPropertyWithDefaultvalue:0.0 minValue:0.0 maxValue:2.0] named:@"state"];	
-	[self addProperty:[BoolProperty boolPropertyWithDefaultvalue:0.0] named:@"clear"];
+	[self addProperty:[NumberProperty sliderPropertyWithDefaultvalue:0.0 minValue:0.0 maxValue:2000] named:@"yMax"];	
+	[self addProperty:[NumberProperty sliderPropertyWithDefaultvalue:0.0 minValue:0.0 maxValue:2000] named:@"goodPointMaxY"];	
+		[self addProperty:[NumberProperty sliderPropertyWithDefaultvalue:0.0 minValue:0.0 maxValue:2.0] named:@"goodBadFactor"];	
 	
-	[self addProperty:[NumberProperty sliderPropertyWithDefaultvalue:5.0 minValue:0.0 maxValue:2.0] named:@"elasticForce"];	
+	
+	[self addProperty:[BoolProperty boolPropertyWithDefaultvalue:0.0] named:@"clear"];
+	[self addProperty:[BoolProperty boolPropertyWithDefaultvalue:1.0] named:@"enableKinect"];
+	
+	[self addProperty:[NumberProperty sliderPropertyWithDefaultvalue:5.0 minValue:0.0 maxValue:0.2] named:@"elasticForce"];	
 	[self addProperty:[NumberProperty sliderPropertyWithDefaultvalue:0.5 minValue:0 maxValue:1.0] named:@"damping"];	
-	[self addProperty:[NumberProperty sliderPropertyWithDefaultvalue:0.03 minValue:0.0 maxValue:0.12] named:@"pullForce"];	
+	[self addProperty:[NumberProperty sliderPropertyWithDefaultvalue:0.03 minValue:0.0 maxValue:0.1] named:@"pullForce"];	
 	[self addProperty:[NumberProperty sliderPropertyWithDefaultvalue:0.03 minValue:0.0 maxValue:10] named:@"speed"];	
+	[self addProperty:[NumberProperty sliderPropertyWithDefaultvalue:1 minValue:0.0 maxValue:10] named:@"border"];	
+	[self addProperty:[NumberProperty sliderPropertyWithDefaultvalue:0 minValue:0 maxValue:0.2] named:@"pushForceInternal"];			
+	[self addProperty:[NumberProperty sliderPropertyWithDefaultvalue:0 minValue:0 maxValue:2] named:@"pushForceExternal"];			
+	[self addProperty:[NumberProperty sliderPropertyWithDefaultvalue:0.2 minValue:0.01 maxValue:1.0] named:@"pushForceInternalDist"];			
+	[self addProperty:[NumberProperty sliderPropertyWithDefaultvalue:0.2 minValue:0.01 maxValue:1.0] named:@"pushForceExternalDist"];			
+	
+	[self addProperty:[NumberProperty sliderPropertyWithDefaultvalue:1 minValue:1.0 maxValue:500] named:@"iterations"];		
+	[self addProperty:[NumberProperty sliderPropertyWithDefaultvalue:100 minValue:1.0 maxValue:500] named:@"KinectRes"];			
+	[self addProperty:[NumberProperty sliderPropertyWithDefaultvalue:1 minValue:1.0 maxValue:10] named:@"filterPasses"];			
+	
 	
 	mouseh = -1;
 	rubbers = [NSMutableArray array];
@@ -135,76 +293,126 @@
 -(void) observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context{
 	if(object == Prop(@"clear") && [object boolValue]){
 		clear = true;
-		[object setBoolValue:NO];
+		[object setBoolValue:NO];		
 	}
 }
 
 
 -(void) update:(NSDictionary *)drawingInformation{
+	goodPoints.clear();
+	badPoints.clear();
+	
 	if(clear){
+
+		[rubbers removeAllObjects];
+		timeout = 100;
 		clear = NO;
 	}
 	
-	vector<ofxPoint3f> points = [GetPlugin(Kinect) getPointsInBoxXMin:0 xMax:[self aspect] yMin:0 yMax:10000 zMin:0 zMax:0.6 res:37];
-	if(points.size() > 0){
-		cout<<points.size()<<endl;
-		vector<ofxPoint2f> points2;
-		for(int i=0;i<points.size();i++){
-			points2.push_back(ofxPoint2f(points[i].x, points[i].z));
+	vector<ofxPoint2f> points;
+	
+	
+	if(PropB(@"enableKinect")){
+		vector<ofxPoint3f> pointsKinect = [GetPlugin(Kinect) getPointsInBoxXMin:0 xMax:[self aspect] yMin:0 yMax:PropI(@"yMax") zMin:0 zMax:0.8 res:PropI(@"KinectRes")];
+		if(pointsKinect.size() > 0){
+			for(int i=0;i<pointsKinect.size();i++){				
+				if(pointsKinect[i].y > PropF(@"goodPointMaxY"))
+					badPoints.push_back(ofxPoint2f(pointsKinect[i].x, pointsKinect[i].z));
+				else {
+					
+					goodPoints.push_back(ofxPoint2f(pointsKinect[i].x, pointsKinect[i].z));	
+				}
+				
+				points.push_back(ofxPoint2f(pointsKinect[i].x, pointsKinect[i].z));
+			}		
+			if(badPoints.size() / (float) goodPoints.size() > PropF(@"goodBadFactor"))
+				points.clear();
 		}
-		
-		
-		Rubber * r;
-		if(timeout > 30){
-			r = [[Rubber alloc] init];
-			[r bind:@"elasticForce" toObject:self withKeyPath:@"properties.elasticForce" options:nil];
-			[r bind:@"damping" toObject:self withKeyPath:@"properties.damping" options:nil];
-			[r bind:@"pullForce" toObject:self withKeyPath:@"properties.pullForce" options:nil];
-			[r bind:@"speed" toObject:self withKeyPath:@"properties.speed" options:nil];
-			
-			[rubbers addObject:r];
-		} else {
-			r = [rubbers lastObject];
-		}
-		[r updateWithPoints:points2];
-		
-		timeout = 0;
-		
-		
 	}
 	
-	
-	if(mouseh >= 0){
-		
-		vector<ofxPoint2f> points;
+	if(mouseh > 0){		
 		points.reserve(100);
 		for(int i=0;i<100;i++){
 			float r = TWO_PI*i/100.0;
 			float s = 0.09;
 			points.push_back(ofxPoint2f(mousex*[self aspect],mousey)+ ofxPoint2f(cos(r)*s,sin(r)*s));
 		}
-		
-		Rubber * r;
-		if(timeout > 30){
-			r = [[Rubber alloc] init];
-			[r bind:@"elasticForce" toObject:self withKeyPath:@"properties.elasticForce" options:nil];
-			[r bind:@"damping" toObject:self withKeyPath:@"properties.damping" options:nil];
-			[r bind:@"pullForce" toObject:self withKeyPath:@"properties.pullForce" options:nil];
-			[r bind:@"speed" toObject:self withKeyPath:@"properties.speed" options:nil];
-			
-			[rubbers addObject:r];
-		} else {
-			r = [rubbers lastObject];
+	} else if(mouseh == 0){
+		for(int i=0;i<100;i++){
+			float r = TWO_PI*i/100.0;
+			float s = 0.09;
+		//	storedPoints.push_back(ofxPoint2f(mousex*[self aspect],mousey)+ ofxPoint2f(cos(r)*s,sin(r)*s));
 		}
-		[r updateWithPoints:points];
 		
+	}
+	
+
+	
+	Rubber * updateRubber;
+	if(points.size() > 0){
+		if(timeout > 30){
+			updateRubber = [[Rubber alloc] initWithPoints:points];
+			[updateRubber bind:@"elasticForce" toObject:self withKeyPath:@"properties.elasticForce" options:nil];
+			[updateRubber bind:@"damping" toObject:self withKeyPath:@"properties.damping" options:nil];
+			[updateRubber bind:@"pullForce" toObject:self withKeyPath:@"properties.pullForce" options:nil];
+			[updateRubber bind:@"speed" toObject:self withKeyPath:@"properties.speed" options:nil];
+			[updateRubber bind:@"border" toObject:self withKeyPath:@"properties.border" options:nil];
+			[updateRubber bind:@"pushForceInternal" toObject:self withKeyPath:@"properties.pushForceInternal" options:nil];
+			[updateRubber bind:@"pushForceExternal" toObject:self withKeyPath:@"properties.pushForceExternal" options:nil];			
+			[updateRubber bind:@"pushForceInternalDist" toObject:self withKeyPath:@"properties.pushForceInternalDist" options:nil];
+			[updateRubber bind:@"pushForceExternalDist" toObject:self withKeyPath:@"properties.pushForceExternalDist" options:nil];			
+			updateRubber->aspect = [self aspect];
+			
+			
+			for(int i=0;i<500;i++){
+				[updateRubber updateWithPoints:points];
+				[updateRubber updateWithTimestep:60.0/ofGetFrameRate()];				
+				[updateRubber calculateFilteredPos];
+				
+			}
+			
+			[rubbers addObject:updateRubber];
+		} else {
+			updateRubber = [rubbers lastObject];
+		}
 		timeout = 0;
 	}
-	timeout ++;
+	
+	
+	
+	if(points.size() > 150){
+		points.erase(points.begin(),points.begin()+points.size()-150);
+	}
 	
 	for(Rubber * r in rubbers){
-		[r updateWithTimestep:60.0/ofGetFrameRate()];
+		[r updateForceToOtherObjects:rubbers];
 	}
+	for(int i=0;i<PropI(@"iterations");i++){
+		for(Rubber * r in rubbers){
+			if(r == updateRubber){
+				[r updateWithPoints:points];
+			}
+			
+			[r updateWithTimestep:60.0/ofGetFrameRate()];
+		}
+		
+	}
+	
+	
+	
+	
+	for(int i=0;i<PropI(@"filterPasses");i++){
+		for(Rubber * r in rubbers){
+			[r 	calculateFilteredPos];
+		}
+	}
+	
+	
+	
+	
+	
+	timeout ++;
+	
 	
 }
 
@@ -214,6 +422,17 @@
 	for(Rubber * r in rubbers){		
 		[r draw];
 	}
+	
+	ofSetColor(0, 255, 0);
+	for(int i=0;i<goodPoints.size();i++){
+		ofRect(goodPoints[i].x, goodPoints[i].y, 0.01, 0.01);
+	}
+	
+	ofSetColor(255, 0, 0);
+	for(int i=0;i<badPoints.size();i++){
+		ofRect(badPoints[i].x, badPoints[i].y, 0.01, 0.01);
+	}
+	
 	PopSurface();
 	
 }
